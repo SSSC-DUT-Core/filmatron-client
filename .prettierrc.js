c5c887bfd627131d89565a664f53e939d0e3ff8f
Truong Nguyen <truongnguyenptn@gmail.com>
2023-09-30 22:59:45 +0700

    fix: config  husky

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+module.exports = {
+    tabWidth: 4,
+    printWidth: 80,
+    endOfLine: "auto",
+    arrowParens: "avoid",
+    trailingComma: "es5",
+    semi: true,
+    useTabs: false,
+    singleQuote: false,
+    bracketSpacing: true,
+};
