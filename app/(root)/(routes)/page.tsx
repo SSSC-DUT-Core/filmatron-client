092b679398e07175ab57d0b587a261fe508c4f8c
kiemtran.dev <kiemtran.dev@gmail.com>
2023-10-02 17:01:02 +0700

    Test cors on deployment

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,6 @@
-"client only  ";
+"use client";
 
+import { useEffect } from "react";
 import { FilmDetails } from "./(homepage)/FilmDetail/index";
 import { FilmRow } from "./(homepage)/FilmRow/index";
 import { LiveFilmSection } from "./(homepage)/LiveFilmSection/index";
@@ -10,6 +11,40 @@ import {
 } from "./(homepage)/data";
 
 const Home = () => {
+    useEffect(() => {
+        if (sessionStorage.getItem("access_token")) {
+            const url = "https://filmatron-jwks.kylan.so/api/user";
+            const body = JSON.stringify({});
+            fetch(url, {
+                method: "GET",
+                // body,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    Authorization: sessionStorage.getItem("access_token") ?? "",
+                },
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    const dataFake = {
+                        _id: "6514fc9e8ee40a22defddc33",
+                        name: "Kiem Tran",
+                        email: "kiemtran.dev@gmail.com",
+                        image: "https://lh3.googleusercontent.com/a/ACg8ocLTT45XT8F4WTzFDECLti2RTGWq4GAxihao79QUMxBre8o=s96-c",
+                        emailVerified: null,
+                    };
+                    console.log("data: ", data);
+                })
+                .catch(error => {
+                    console.error("Fetch error:", error);
+                });
+        }
+    }, []);
+
     return (
         <>
             <FilmDetails />
