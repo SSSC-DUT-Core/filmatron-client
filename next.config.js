b8ebab22f6f69902ccecaca52cee44b7e909b1f9
kiemtran.dev <kiemtran.dev@gmail.com>
2023-10-20 01:16:55 +0700

    fix build fail

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "res.cloudinary.com",
-            "www.cgv.vn",
-            "arweave.net",
-            "supersweetcollection.notarealurl",
-            "media.vov.vn",
-            "encrypted-tbn0.gstatic.com",
-            "images4.alphacoders.com",
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
         ],
     },
 };
