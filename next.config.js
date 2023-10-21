5678f45df93f06d5eb60f09f5149c0b27d13f8a7
kiemtran.dev <kiemtran.dev@gmail.com>
2023-10-21 17:06:56 +0700

    handle loading

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "res.cloudinary.com",
-            "www.cgv.vn",
-            "arweave.net",
-            "encrypted-tbn0.gstatic.com",
-            "supersweetcollection.notarealurl",
-            "media.vov.vn",
-            "images4.alphacoders.com",
-            "lh3.googleusercontent.com",
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
         ],
     },
 };
